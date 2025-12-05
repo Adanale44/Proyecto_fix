61f7fc858a5e776ce562db60ac8f4efaaa7145d3
benjamin martinez <benjamartinez454gmail.com>
2025-12-05 14:30:25 -0300

    cambio para al correr un test

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,21 +34,22 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+  /*
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+*/
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+/*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -71,9 +72,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+webServer: {
+  command: 'npm run dev',
+  timeout: 120 * 1000, // Tiempo máximo de espera
+  url: 'http://localhost:3000',
+  reuseExistingServer: !process.env.CI,
+  },
 });
